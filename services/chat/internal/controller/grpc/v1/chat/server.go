cde9e516a0899c1ffa1058a4d5a7301697cad9b2
Nick Nazarov <nn@webizi.ru>
2025-04-29 14:46:13 +0300

    [MSA-14] feat(chat): implement a gRPC server and HTTP server without business logic for the Chat microservice

--- services/chat/internal/controller/grpc/v1/chat/server.go
+++ services/chat/internal/controller/grpc/v1/chat/server.go
@@ -2,30 +2,61 @@ package chat
 
 import (
 	"context"
+	"fmt"
+	"github.com/bufbuild/protovalidate-go"
 	pb "github.com/nazarovnick/chat-platform/services/chat/pkg/api/grpc/chat"
+	"log"
 )
 
 type server struct {
 	pb.UnimplementedChatServiceServer
+	validator protovalidate.Validator
 }
 
-func NewServer() pb.ChatServiceServer {
-	return &server{}
+func NewServer() (pb.ChatServiceServer, error) {
+	srv := &server{}
+
+	validator, err := protovalidate.New(
+		protovalidate.WithDisableLazy(),
+		protovalidate.WithMessages(
+			&pb.SendMessageRequest{},
+			&pb.SendMessageResponse{},
+			&pb.GetMessagesRequest{},
+			&pb.GetMessagesResponse{},
+		),
+	)
+
+	if err != nil {
+		return nil, fmt.Errorf("failed to initialize validator: %w", err)
+	}
+
+	srv.validator = validator
+
+	return srv, nil
 }
 
-func (s *server) SendMessage(ctx context.Context, req *pb.SendMessageRequest) (*pb.SendMessageResponse, error) {
+func (s *server) SendMessage(_ context.Context, req *pb.SendMessageRequest) (*pb.SendMessageResponse, error) {
+	log.Printf("Message sent to chat with ID %v from user with ID %v", req.GetChatId(), req.GetUserId())
+	if err := s.validator.Validate(req); err != nil {
+		return nil, rpcValidationError(err)
+	}
 	return &pb.SendMessageResponse{
-		MessageId: "testID",
+		MessageId: 777,
 	}, nil
 }
 
-func (s *server) GetMessages(ctx context.Context, req *pb.GetMessagesRequest) (*pb.GetMessagesResponse, error) {
+func (s *server) GetMessages(_ context.Context, req *pb.GetMessagesRequest) (*pb.GetMessagesResponse, error) {
+	log.Printf("Get messages from chat ID %v", req.GetChatId())
+	if err := s.validator.Validate(req); err != nil {
+		return nil, rpcValidationError(err)
+	}
+
 	return &pb.GetMessagesResponse{
 		Messages: []*pb.Message{
 			{
-				Id:        "testID",
-				ChatId:    "testChatID",
-				UserId:    "testUserID",
+				Id:        777,
+				ChatId:    888,
+				UserId:    999,
 				Content:   "testContent",
 				Timestamp: 3000,
 			},
--- services/chat/internal/controller/http/router.go
+++ services/chat/internal/controller/http/router.go
@@ -1,16 +1,14 @@
 package http
 
 import (
-	"github.com/gofiber/fiber/v2"
-	v1 "github.com/nazarovnick/chat-platform/services/auth/internal/controller/http/v1"
+	"github.com/gorilla/mux"
+	"net/http"
 )
 
-// NewRouter creates a new Fiber application, groups API routes under specified prefix (e.g. "/api"),
-// and registers each version’s routes (e.g., v1, v2, etc.).
-func NewRouter() *fiber.App {
-	app := fiber.New()
-	api := app.Group("/api")
-	v1.RegisterRoutes(api.Group("/v1"))
+func NewRouter(gwMux http.Handler) *mux.Router {
+	router := mux.NewRouter()
+	RegisterHealthRoutes(router)
+	router.PathPrefix("/").Handler(gwMux)
 
-	return app
+	return router
 }
