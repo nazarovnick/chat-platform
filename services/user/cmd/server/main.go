385d698e2ee7ccaf3006defd6014ea2b0557b7a9
Никита Назаров <justmove8@gmail.com>
2025-05-04 23:53:00 +0300

    [MSA-16] feat(user): implement a server without business logic and add Swagger UI

--- services/user/cmd/server/main.go
+++ services/user/cmd/server/main.go
@@ -1,16 +1,65 @@
 package main
 
 import (
-	"github.com/nazarovnick/chat-platform/services/auth/internal/controller/http"
+	"context"
+	"github.com/grpc-ecosystem/grpc-gateway/v2/runtime"
+	"github.com/nazarovnick/chat-platform/services/user/internal/controller/grpc/v1/user"
+	"github.com/nazarovnick/chat-platform/services/user/internal/controller/http"
+	pb "github.com/nazarovnick/chat-platform/services/user/pkg/api/grpc/user/v1"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/reflection"
 	"log"
+	"net"
+	"sync"
 )
 
 // main initializes the HTTP router and starts the server on specified port
 func main() {
-	app := http.NewRouter()
-	addr := ":8080"
-	log.Printf("Server running at %s", addr)
-	if err := app.Listen(addr); err != nil {
-		log.Fatalf("Failed to start server: %v", err)
+
+	ctx := context.Background()
+	ctx, cancel := context.WithCancel(ctx)
+	defer cancel()
+
+	impl, err := user.NewServer()
+	if err != nil {
+		log.Fatalf("failed to create impl: %v", err)
 	}
+
+	var wg sync.WaitGroup
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		grpcServer := grpc.NewServer()
+		pb.RegisterUserServiceServer(grpcServer, impl)
+		reflection.Register(grpcServer)
+		lis, err := net.Listen("tcp", ":8091")
+		if err != nil {
+			log.Fatalf("failed to listen: %v", err)
+		}
+		log.Printf("gRPC impl listening at %v", lis.Addr())
+		if err := grpcServer.Serve(lis); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+	}()
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		gwMux := runtime.NewServeMux()
+
+		if err = pb.RegisterUserServiceHandlerServer(ctx, gwMux, impl); err != nil {
+			log.Fatalf("failed to serve: %v", err)
+		}
+
+		httpServer := http.NewRouter(gwMux)
+		addr := ":8080"
+		log.Printf("HTTP server running at %s", addr)
+		if err := httpServer.Listen(addr); err != nil {
+			log.Fatalf("Failed to start impl: %v", err)
+		}
+	}()
+
+	wg.Wait()
 }
--- services/user/internal/controller/http/v1/router.go
+++ services/user/internal/controller/http/v1/router.go
@@ -1,9 +1,14 @@
 package v1
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"net/http"
+)
 
 // RegisterRoutes registers all application-specific API routes for version 1
 // on the provided Fiber router.
-func RegisterRoutes(router fiber.Router) {
+func RegisterRoutes(router fiber.Router, gwMux http.Handler) {
+	RegisterSwaggerRoutes(router)
 	RegisterHealthRoutes(router)
+	RegisterGrpcGatewayRoutes(router, gwMux)
 }
